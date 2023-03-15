37303ccc0eaa3cfa91bf43a807d1291fa69b3f27
Andrey Marakulin <annndruha.github@gmail.com>
2023-03-15 07:22:23 +0300

    Update base.py

--- temperature_monitor_api/routes/base.py
+++ temperature_monitor_api/routes/base.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -35,12 +37,15 @@ app.add_middleware(
     allow_headers=settings.CORS_ALLOW_HEADERS,
 )
 
+app.include_router(measurements_router, prefix='', tags=['measurements'])
+app.include_router(devices_router, prefix='', tags=['devices'])
+
 
 @app.get("/")
 def serve_home(request: Request):
     return FileResponse("./static/index.html")
 
 
-app.include_router(measurements_router, prefix='', tags=['measurements'])
-app.include_router(devices_router, prefix='', tags=['devices'])
-app.mount('/static', StaticFiles(directory='./static', html=True), 'static')
+parent_dir_path = os.path.dirname(os.path.realpath(__file__))
+print('WORKDIR parent_dir_path: ', parent_dir_path)
+app.mount('/static', StaticFiles(directory='../static', html=True), 'static')
