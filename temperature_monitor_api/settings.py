aecacfbc85b5c2344064bef9cc73163f61f0cbce
Andrey Marakulin <annndruha.github@gmail.com>
2023-09-07 21:28:00 +0300

    Update pydantic

--- temperature_monitor_api/settings.py
+++ temperature_monitor_api/settings.py
@@ -1,13 +1,14 @@
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     """Application settings"""
 
-    DB_DSN: PostgresDsn
+    DB_DSN: str
     ROOT_PATH: str = '/' + os.getenv("APP_NAME", "")
     ADMIN_TOKEN: str
 
@@ -16,11 +17,7 @@ class Settings(BaseSettings):
     CORS_ALLOW_METHODS: list[str] = ['*']
     CORS_ALLOW_HEADERS: list[str] = ['*']
 
-    class Config:
-        """Pydantic BaseSettings config"""
-
-        case_sensitive = True
-        env_file = ".env"
+    model_config = ConfigDict(case_sensitive=True, env_file=".env", extra="allow")
 
 
 @lru_cache
