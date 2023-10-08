f5b932cb0a3572fa025785121954b03d4a10ba5d
Andrey Marakulin <annndruha.github@gmail.com>
2023-10-08 21:34:41 +0300

    migration

---
+++ migrations/versions/a6ca37b42495_datetime.py
@@ -0,0 +1,24 @@
+"""datetime
+
+Revision ID: a6ca37b42495
+Revises: e0be41d325e3
+Create Date: 2023-10-08 21:32:54.235512
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a6ca37b42495'
+down_revision = 'e0be41d325e3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
